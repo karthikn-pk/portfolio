87aa85b75fca43bc5a2af4c5fc03b84578cbe1c5
karthikn-pk <pkarthikeyanp33@gmail.com>
2024-04-03 17:29:29 +0530

    skills added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div>
+      <h1 className="text-2xl">FrontEnd Technologies</h1>
+      <ul className="flex p-4 ">
+        <li className="m-4">
+          <img
+            className=" w-52"
+            src="https://msatechnosoft.in/blog/wp-content/uploads/2017/04/Web-Design-using-HTML-msa-technosoft.png"
+            alt=""
+          />
+        </li>
+        <li className="m-4">
+          <img
+            className=" w-52"
+            src="https://1000logos.net/wp-content/uploads/2020/09/CSS-Logo.jpg"
+            alt=""
+          />
+        </li>
+        <li className="m-4">
+          <img
+            className=" w-52"
+            src="https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tailwindcss.png"
+            alt=""
+          />
+        </li>
+        <li className="m-4">
+          <img
+            className="w-52"
+            src="https://qph.cf2.quoracdn.net/main-qimg-d24933a6c63057c57c49521e3363a929"
+            alt=""
+          />
+        </li>
+        <li className="m-4">
+          <img
+            className=" w-52"
+            src="https://amani.home.blog/wp-content/uploads/2018/12/javascript-logo.png"
+            alt=""
+          />
+        </li>
+
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Skills;
